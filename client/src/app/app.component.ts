992effa7b93c56c0d39696e7691176b74a3629aa
Eldar Absalyamov <salyamkin@gmail.com>
2021-01-21 18:10:49 +0600

    end of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component } from '@angular/core';
-import { IPagination } from './models/IPagination';
-import { IProduct } from './models/IProduct';
+
 
 @Component({
   selector: 'app-root',
@@ -10,16 +9,10 @@ import { IProduct } from './models/IProduct';
 })
 export class AppComponent {
   title = 'Skinet';
-  products: IProduct[];
+
  
-  constructor(private http: HttpClient) {}
+  constructor() {}
  
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
-  }
+  
 
 }
