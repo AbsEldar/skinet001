0a9270233066af0ec2af9514c2aab57278c4528c
Eldar Absalyamov <salyamkin@gmail.com>
2021-01-24 02:21:32 +0600

    end of lesson 166

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,6 +54,7 @@ namespace API
 
             
             services.AddApplicationServices();
+            services.AddIdentityServices();
             services.AddSwaggerDocumentation();
 
               services.AddCors(opt => {
