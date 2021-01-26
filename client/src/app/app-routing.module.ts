9ffbffb0c09ffec7c49fe1ed655841ca35830cf3
Eldar Absalyamov <salyamkin@gmail.com>
2021-01-27 00:32:25 +0600

    end of lesson 186

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
   {path: 'checkout', loadChildren: () => import('./checkout/checkout.module')
     .then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}},
+
+  {path: 'account', loadChildren: () => import('./account/account.module')
+    .then(mod => mod.AccountModule), data: {breadcrumb: {skip: true}}},
+  
   // {path: 'shop/:id', component: ProductDetailsComponent},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
